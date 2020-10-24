d849dce469b4a5343c88875ed28b93bc12934133
DawidMalarz <dawidmalarz@pop-os.localdomain>
2020-10-24 21:09:31 +0200

    Change project structure

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import api
-import pandas as pd
+from scrapers import songs_list
+import yaml
 
-ARTIST = []
-api.get_data_from_genius(artist_names=ARTIST, count=None)
+if __name__ == "__main__":
+
+    with open('artists.yaml') as file:
+        yaml_data = yaml.load(file, Loader=yaml.FullLoader)
+
+    for artist in yaml_data['Artists']:
+        songs_list.get_songs(artist, 3)
